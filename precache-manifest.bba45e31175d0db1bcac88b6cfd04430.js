e74e09b4c20b3cbffcbf39abb65d868a70a7261d
Divyanshu Rai <divyanshu1997@gmail.com>
2020-02-06 04:12:57 +0530

    Updates

---
+++ precache-manifest.bba45e31175d0db1bcac88b6cfd04430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c9476094e8ff2d30907f7a6853da8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886b5a7c96ade671956d",
+    "url": "/static/css/main.dc3a655d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "886b5a7c96ade671956d",
+    "url": "/static/js/main.050aca46.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
